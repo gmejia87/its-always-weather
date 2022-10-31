653a9c644ca8577f7464c6ea33f36b730b064a3d
G Codes <graciela.mejia1@yahoo.com>
2022-10-31 13:46:47 -0700

    update to bootstrap, divs/add script.js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var city = document.querySelector("#search-city");
+var savedCities = document.querySelector("#city-results");
+var weather = document.querySelector("#current-weather");
+var forecast = document.querySelector("#five-day-forecast");
