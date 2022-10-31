5bc875894c759eaad691901a5e5a91f4802bb780
G Codes <graciela.mejia1@yahoo.com>
2022-10-31 14:00:42 -0700

    update to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ var city = document.querySelector("#search-city");
 var savedCities = document.querySelector("#city-results");
 var weather = document.querySelector("#current-weather");
 var forecast = document.querySelector("#five-day-forecast");
+
+var getCitySearch = function (city) {
+  //format weather api
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=a78bb3546c41bd73b925fe7a495e1e89";
+};
+
+//event listeners
